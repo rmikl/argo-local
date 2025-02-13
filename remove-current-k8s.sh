6617efdbc6b48eb34c9bb28996c2f7150e82dd24
rmikl <robert.miklas@protonmail.com>
2025-02-13 15:02:01 +0100

    added deletion script

--- remove-current-k8s.sh
+++ remove-current-k8s.sh
@@ -23,3 +23,31 @@ sudo apt-get install -y kubeadm kubelet kubectl
 
 
 sudo docker system prune -af || sudo crictl rmi --prune
+
+sudo kubeadm reset -f
+
+# Remove any remaining Kubernetes configuration/files
+sudo rm -rf /etc/cni/net.d
+sudo rm -rf $HOME/.kube/config
+sudo rm -rf /etc/kubernetes
+
+# Clean up CNI interfaces and iptables rules (optional but recommended)
+sudo ip link delete cni0
+sudo ip link delete flannel.1
+sudo iptables -F
+sudo iptables -t nat -F
+sudo iptables -t mangle -F
+sudo iptables -X
+
+# Remove Kubernetes packages (optional)
+sudo apt-get purge kubeadm kubelet kubectl -y
+sudo apt-get autoremove -y
+
+# Reinstall Kubernetes tools (if needed)
+sudo apt-get install -y kubeadm kubelet kubectl
+
+
+sudo docker system prune -af || sudo crictl rmi --prune
+
+
+sudo reboot 0
